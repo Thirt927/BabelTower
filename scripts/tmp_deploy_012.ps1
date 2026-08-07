5df777cd398f106f573b6a81c6417920b7d35459
c1375rick <c1375rick@users.noreply.github.com>
2026-08-07 20:58:51 +0800

    chore: cleanup tmp probe scripts

--- scripts/tmp_deploy_012.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-$vpk = "<PROJECT_DIR>\dist\pak01_dir.vpk"
-$addons = "<GAME_LIB>\steamapps\common\Deadlock\game\citadel\addons\pak11_dir.vpk"
-$dmm = "C:\Users\kali1\AppData\Local\dev.stormix.deadlock-mod-manager\mods\local-9a893075-8d63-4058-9fbe-406f3b2d395b\files\pak01_dir.vpk"
-Copy-Item -Path $vpk -Destination $addons -Force
-Copy-Item -Path $vpk -Destination $dmm -Force
-Get-FileHash $vpk, $addons, $dmm | ForEach-Object { "$($_.Path.Split('\')[-3..-1] -join '\'): $($_.Hash)" }
