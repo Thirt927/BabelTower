c2b4f3ee7ec673a0bd8d6994a89b77a505401ae6
c1375rick <c1375rick@users.noreply.github.com>
2026-08-07 20:53:30 +0800

    release: 0.1.2 stable (settings panel footer fix + outgoing dispatch timeout)

---
+++ scripts/tmp_deploy_012.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = "Stop"
+$vpk = "F:\BabelTower\dist\pak01_dir.vpk"
+$addons = "F:\SteamLibrary\steamapps\common\Deadlock\game\citadel\addons\pak11_dir.vpk"
+$dmm = "C:\Users\kali1\AppData\Local\dev.stormix.deadlock-mod-manager\mods\local-9a893075-8d63-4058-9fbe-406f3b2d395b\files\pak01_dir.vpk"
+Copy-Item -Path $vpk -Destination $addons -Force
+Copy-Item -Path $vpk -Destination $dmm -Force
+Get-FileHash $vpk, $addons, $dmm | ForEach-Object { "$($_.Path.Split('\')[-3..-1] -join '\'): $($_.Hash)" }
